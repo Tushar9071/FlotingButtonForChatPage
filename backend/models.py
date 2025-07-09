19d83d984a2d7a37ac13c648a3e287ac5e8bdd03
Tushar Rajpara <tusharrajpara00@gmail.com>
2025-07-09 19:15:58 +0530

    09-07-2025

---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import String
+from .database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
