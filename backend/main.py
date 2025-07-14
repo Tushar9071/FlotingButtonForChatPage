192ea7d6e0869f31df21578c77d60c7e1e6049a0
Tushar Rajpara <tusharrajpara00@gmail.com>
2025-07-14 17:52:03 +0530

    14-07-2025

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException, status,Path
 from sqlalchemy.orm import Session
 from . import models, database , schemas
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
 
 models.Base.metadata.create_all(bind=database.engine)
 
@@ -13,62 +15,35 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/users")
-async def read_users(db: Session = Depends(get_db)):
-    return {"data":db.query(models.User).all()}
+@app.post('/api/add/user')
+async def add_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # print(user)
+    # Check if email or phone already exists
+    existing_user = (
+        db.query(models.User)
+        .filter(
+            (models.User.email == user.email) |     
+            (models.User.phone == user.phone)
+        )
+        .first()
+    )
 
-@app.post('/users',response_model=schemas.UserResponse)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # Check if email already exists
-    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
     if existing_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already registered",
+            detail="User with this email or phone already exists"
         )
-    
-    # Create the user
-    db_user = models.User(name=user.name, email=user.email)
-    db.add(db_user)
+
+    new_user = models.User(email=user.email, phone=user.phone , data = user.data)
+    db.add(new_user)
     db.commit()
-    db.refresh(db_user)
-    return db_user
-
-
-@app.put("/users/{user_id}", response_model=schemas.UserResponse)
-async def update_user(
-    user_update: schemas.UserUpdate,
-    user_id: int = Path(..., title="The ID of the user to update"),
-    db: Session = Depends(get_db)
-):
-    db_user = db.query(models.User).filter(models.User.id == user_id).first()
-
-    if not db_user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with id {user_id} not found"
-        )
-    if user_update.name is not None:
-        db_user.name = user_update.name
-    if user_update.email is not None:
-        db_user.email = user_update.email
-    
-    db.commit()
-    db.refresh(db_user)
-
-    return db_user
-
-@app.delete("/users/{user_id}")
-async def delete_user(user_id:int = Path(..., title="The ID of the user to delete"),db:Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.id == user_id).first()
-
-    if not db_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with id {user_id} not found")
-    db.delete(db_user)
-    db.commit()
-
-    return {"message":f"user with id {user_id} deleted"}
+    db.refresh(new_user)
+    return new_user
